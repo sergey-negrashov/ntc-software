679fc6e9ce9d24d6331d603403396f5e3f783576
nathanknshg <nathanknshg@gmail.com>
2016-09-25 17:01:19 -1000

    test file that prints commands

---
+++ motor/test.py
@@ -0,0 +1,21 @@
+class moto:
+    def moveUp(self, numSteps):
+        cmd = 'C E I1M' + str(numSteps) + ',R'
+        print(cmd)
+    def zero(self):
+        print('C E I1M-0, R')
+    def absmove(self, pos):
+        cmd = 'C E IA1M' + str(pos) + ',R'
+        print(cmd)
+    def paus(self, time):
+        cmd = 'C E P' + str(time) + ',R'
+        print(cmd)
+    def loop(self, sec, steps, loops):
+        cmd = 'C E P' + str(sec*10) + ',I1M' + str(steps) + ',L' + str(loops) + ',R'
+        print(cmd)
+
+
+a = moto()
+a.zero()
+a.loop(20,21,5)
+
